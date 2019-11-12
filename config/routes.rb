35569f130bc09afd5327fed7ca7e95e5fa20d751
Coralie Loheac <coralie.loheac@gmail.com>
2019-11-12 15:05:02 +0100

    ex2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # create
+  post 'tasks', to: 'tasks#create'
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
